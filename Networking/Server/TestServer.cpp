f88e2ae004e0b003ba0a36b70ade0b23fa196bba
Evrard Dessain <edessain@fu-r2-p5.s19.be>
2021-08-16 18:58:29 +0200

    implementation of select - probleme reading

--- Networking/Server/TestServer.cpp
+++ Networking/Server/TestServer.cpp
@@ -15,7 +15,8 @@ void TestServer::accepter()
     struct sockaddr_in address = get_socket()->get_address();
     int addrlen = sizeof(address);
     _new_socket = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen); 
-    read(_new_socket, _buffer, 30000);
+    // return (_new_socket);
+    // read(_new_socket, _buffer, 30000);
 }
 
 void TestServer::handler()
@@ -30,6 +31,25 @@ void TestServer::handler()
     test.Http_response::build_http_response(http_req_struct);
 }
 
+void TestServer::readsocket()
+{
+	long valread;
+    int msgsize = 0;
+
+    while ((valread = read(_new_socket, _buffer + msgsize, sizeof(_buffer) - msgsize - 1)))
+	{
+		msgsize += valread;
+		if (msgsize > 1024 - 1 || _buffer[msgsize - 1] == '\n')
+			break ;
+	}
+	_buffer[msgsize - 1] = 0;
+	std::cout << _buffer << std::endl;
+	if (valread < 0)
+	{
+		std::cout << "No bytes are there to read" << std::endl;
+	}
+}
+
 void TestServer::responder()
 {
     const char *hello = "Hello from server";
@@ -41,29 +61,56 @@ void TestServer::launch()
 {
     fd_set ready_sockets;
     fd_set current_socket;
+    int ret;
+
+    // fd_set writing_socket;
+    // fd_set reading_socket;
+
     FD_ZERO(&current_socket);
 	FD_SET(get_socket()->get_sock(), &current_socket);
-    
+
+
     while(true)
     {
         ready_sockets = current_socket;
-		if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0)
+		if ((ret = select(FD_SETSIZE, &ready_sockets, nullptr, nullptr, nullptr)) < 0)
 		{
-			std::cout << "Failure whit select " << std::endl;
+			std::cout << "ret = " << ret << std::endl;
+            std::cout << "Failure with select " << std::endl;
 			exit(EXIT_FAILURE);
 		}
         for (int i = 0; i < FD_SETSIZE; i++)
         {
+            std::cout << i << std::endl;
             if (FD_ISSET(i, &ready_sockets))
             {
+                std::cout << "=========== Waiting ==========" << std::endl;
                 if (i == get_socket()->get_sock())
                 {
-                    std::cout << "=========== Waiting ==========" << std::endl;
+                    // this is a new connection
+                    std::cout << "new socket before connection: " << _new_socket << std::endl;
+                    std::cout << "=========== New connection ==========" << std::endl;
                     accepter();
-                    handler();
-                    responder();
-                    std::cout << "=========== Done ==========" << std::endl;
+                    // readsocket();
+                    // handler();
+                    // responder();
+                    FD_SET(_new_socket, &current_socket);
                 }
+                else 
+                {
+                    // do whatever we do with the connection
+                    std::cout << "socket after connection and before doing something: " << _new_socket << "\n";
+                    std::cout << "=========== doing somethings ==========" << std::endl;
+                    std::cout << " step 0" << std::endl;
+                    readsocket();
+                    std::cout << " step 1" << std::endl;
+                    handler();
+                    std::cout << " step 2" << std::endl;
+                    responder();
+                    std::cout << " step 3" << std::endl;
+                    FD_CLR(i, &current_socket);
+                }
+                std::cout << "=========== Done ==========" << std::endl;
             }
         }
     }
--- Networking/Server/TestServer.hpp
+++ Networking/Server/TestServer.hpp
@@ -12,8 +12,11 @@ class TestServer : public SimpleServer
     private:
         char _buffer[30000];
         int _new_socket;
+        // fd_set writing_socket;
+        // fd_set reading_socket;
         
         void accepter();
+        void readsocket();
         void handler();
         void responder();
 
