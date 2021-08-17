1a5448fb08ea0143e967c8feb9f805d910a3c3c0
Evrard Dessain <edessain@fu-r2-p5.s19.be>
2021-08-17 17:09:41 +0200

    implementation of select - should be good

--- Networking/Server/TestServer.cpp
+++ Networking/Server/TestServer.cpp
@@ -1,3 +1,5 @@
+// probleme de read et transformer _new_socket en fd_set
+
 #include "TestServer.hpp"
 
 TestServer::TestServer() : SimpleServer(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 10)
@@ -10,20 +12,31 @@ TestServer::TestServer(int port) : SimpleServer(AF_INET, SOCK_STREAM, 0, port, I
     launch();
 }
 
+void TestServer::set_socket(int new_socket)
+{
+    _new_socket = new_socket;
+}
+
+int TestServer::get_new_socket()
+{
+    return (_new_socket);
+}
+
 void TestServer::accepter()
 {
+    // int sock;
     struct sockaddr_in address = get_socket()->get_address();
     int addrlen = sizeof(address);
-    _new_socket = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen); 
-    // return (_new_socket);
-    // read(_new_socket, _buffer, 30000);
+    set_socket(accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen));
+    // sock = accept(get_socket()->get_sock(), (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    // recv(sock, _buffer, 2048, 0);
 }
 
 void TestServer::handler()
 {
     std::cout << _buffer << std::endl;
 
-	t_http_request	http_req_struct;
+    t_http_request  http_req_struct;
     std::string     http_response_firstline;
     http_request_parser(_buffer, http_req_struct);
 
@@ -33,28 +46,31 @@ void TestServer::handler()
 
 void TestServer::readsocket()
 {
-	long valread;
+    long ret;
     int msgsize = 0;
 
-    while ((valread = read(_new_socket, _buffer + msgsize, sizeof(_buffer) - msgsize - 1)))
-	{
-		msgsize += valread;
-		if (msgsize > 1024 - 1 || _buffer[msgsize - 1] == '\n')
-			break ;
-	}
-	_buffer[msgsize - 1] = 0;
-	std::cout << _buffer << std::endl;
-	if (valread < 0)
-	{
-		std::cout << "No bytes are there to read" << std::endl;
-	}
+    if ((ret = recv(get_new_socket(), _buffer, 2048, 0)) == -1)
+    {
+        std::cout << "Error with recv" << std::endl;
+
+    }
+    else if (ret == 0)
+    {
+        std::cout << "No bytes are there to read" << std::endl;
+        // should close connection
+    }
+    else
+    {
+        _buffer[msgsize - 1] = 0;
+        std::cout << _buffer << std::endl;
+    }
 }
 
 void TestServer::responder()
 {
     const char *hello = "Hello from server";
-    write(_new_socket, hello, strlen(hello));
-    close(_new_socket);
+    write(get_new_socket(), hello, strlen(hello));
+    close(get_new_socket());
 }
 
 void TestServer::launch()
@@ -67,47 +83,41 @@ void TestServer::launch()
     // fd_set reading_socket;
 
     FD_ZERO(&current_socket);
-	FD_SET(get_socket()->get_sock(), &current_socket);
+    FD_SET(get_socket()->get_sock(), &current_socket);
 
 
     while(true)
     {
         ready_sockets = current_socket;
-		if ((ret = select(FD_SETSIZE, &ready_sockets, nullptr, nullptr, nullptr)) < 0)
-		{
-			std::cout << "ret = " << ret << std::endl;
+        if ((ret = select(FD_SETSIZE, &ready_sockets, nullptr, nullptr, nullptr)) < 0)
+        {
             std::cout << "Failure with select " << std::endl;
-			exit(EXIT_FAILURE);
-		}
+            exit(EXIT_FAILURE);
+        }
         for (int i = 0; i < FD_SETSIZE; i++)
         {
-            std::cout << i << std::endl;
-            if (FD_ISSET(i, &ready_sockets))
+            if (FD_ISSET(i, &current_socket))
             {
                 std::cout << "=========== Waiting ==========" << std::endl;
                 if (i == get_socket()->get_sock())
                 {
                     // this is a new connection
-                    std::cout << "new socket before connection: " << _new_socket << std::endl;
                     std::cout << "=========== New connection ==========" << std::endl;
                     accepter();
-                    // readsocket();
-                    // handler();
-                    // responder();
-                    FD_SET(_new_socket, &current_socket);
+                    FD_SET(get_new_socket(), &current_socket);
                 }
-                else 
+                else
                 {
                     // do whatever we do with the connection
-                    std::cout << "socket after connection and before doing something: " << _new_socket << "\n";
+                    // std::cout << "socket after connection and before doing something: " << get_new_socket() << "\n";
                     std::cout << "=========== doing somethings ==========" << std::endl;
-                    std::cout << " step 0" << std::endl;
+                    // std::cout << " step 0" << std::endl;
                     readsocket();
-                    std::cout << " step 1" << std::endl;
+                    // std::cout << " step 1" << std::endl;
                     handler();
-                    std::cout << " step 2" << std::endl;
+                    // std::cout << " step 2" << std::endl;
                     responder();
-                    std::cout << " step 3" << std::endl;
+                    // std::cout << " step 3" << std::endl;
                     FD_CLR(i, &current_socket);
                 }
                 std::cout << "=========== Done ==========" << std::endl;
